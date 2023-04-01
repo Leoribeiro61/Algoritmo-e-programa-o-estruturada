f49b99871257c55fc585d8850d3141b9fdfc44b6
leoribeiro61 <leonardo4600@gmail.com>
2023-03-31 21:32:06 -0300

    Atividade_Lista3

--- Lista003/ex4.c
+++ Lista003/ex4.c
@@ -6,44 +6,55 @@ Menor que 0 não nasceu
 18-65 obrigatório
 exatamente 41- somente ganha prêmio e não vota!!!
 > 65 opcional
-exatamente 88 - somente ganha prêmio e não vota!!!*/
+exatamente 88 - somente ganha prêmio e não vota!!!
 
+- Não utilizar os operadores lógicos || ou &&
+- Utilizar somente estrutura if/else conforme apresentada abaixo
+- Não utilizar switch/case
+- Não utilizar a estrutura "else if"
+
+*/
 
 #include <stdio.h>
 
 int main() {
-   int idade;
-
-   printf("Digite a sua idade: ");
-   scanf("%d", &idade);
-
-   if (idade < 0) {
-    printf("Voce ainda nao nasceu.\n");
-    }
-
-        if (idade >= 0 && idade <= 15) {
-        printf("Voce ainda nao pode votar.\n");
-        }
-
-            if (idade >= 16 && idade <= 17) {
-            printf("Seu voto e opcional.\n");
-            }
-
-                if (idade >= 18 && idade <= 65) {
-                printf("Seu voto e obrigatorio.\n");
-                }
-
-                    if (idade == 41) {
-                    printf("Voce ganhou um premio, mas nao pode votar.\n");
-                    }
-
-                        if (idade > 65 && idade != 88) {
-                        printf("Seu voto e opcional.\n");
-                        }
-
-                            if (idade == 88) {
-                            printf("Voce ganhou um premio, mas nao pode votar.\n");
-                            }
-
-   return 0;
+	
+	int idade = 0;
+    
+	printf("----------------------- \n");
+	printf("  SITUACAO     ELEITORAL \n");
+	printf("----------------------- \n");
+	
+	printf("  Informe a sua idade: \n ");
+	scanf("%d",&idade);
+	
+	
+	
+	if(idade < 0){
+		printf("ainda nao nasceu \n");
+	}else{
+		if(idade < 16){
+			printf("Voce ainda nao pode votar \n");
+		}else{
+			if(idade < 18){
+				printf("Seu voto e opcional \n");
+			}else{
+				if(idade < 66){
+					if(idade == 41){
+						printf("Voce ganhou um premio, mas nao pode votar \n");
+					}else{
+						printf("Seu voto e obrigatorio \n");
+					}
+				}else{
+					if(idade == 88){
+						printf("Voce ganhou um premio, mas nao pode votar \n");
+					}else{
+						printf("Seu voto e opcional \n");
+					}
+				}
+			}
+		}
+	}
+		
+	return 0;
 }
